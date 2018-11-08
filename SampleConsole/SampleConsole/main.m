f65baec1bb7112c923c604b4b602d1a826b17ca4
Pablo <pablo.alegre.berges@gmail.com>
2018-11-08 17:14:35 +0100

    First commit sample.

--- SampleConsole/SampleConsole/main.m
+++ SampleConsole/SampleConsole/main.m
@@ -11,7 +11,7 @@
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"Hello, World! This is synchronized to GitHub!!!");
     }
     return 0;
 }
