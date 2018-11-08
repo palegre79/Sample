a79939d2e29465d92c6787fa2ff8314e5fef54dd
Pablo Alegre <pablo.alegre.berges@gmail.com>
2018-11-08 17:05:38 +0100

    Empty Sample Application

---
+++ SampleConsole/SampleConsole/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  SampleConsole
+//
+//  Created by Pablo on 08/11/2018.
+//  Copyright © 2018 Pablo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
